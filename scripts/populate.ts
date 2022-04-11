a8124fb7eb38a30e1e6cb8d4ed9fabbc988b5dcd
godspeed7 <pawelpikus7@gmail.com>
2022-04-11 18:06:05 +0200

    first commit

---
+++ scripts/populate.ts
@@ -0,0 +1,42 @@
+import {
+  ApolloClient,
+  ApolloQueryResult,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import {
+  LatestProductsDocument,
+  LatestProductsQuery,
+} from "~/generated/saleor";
+
+//@ts-ignore
+const Typesense = require("typesense");
+
+const link = createHttpLink({
+  uri: "https://vercel.saleor.cloud/graphql/",
+  fetch,
+});
+
+const client = new ApolloClient({
+  uri: "https://vercel.saleor.cloud/graphql/",
+  cache: new InMemoryCache(),
+  ssrMode: true,
+});
+
+const main = async () => {
+  const data: ApolloQueryResult<LatestProductsQuery | undefined> =
+    await client.query({
+      query: LatestProductsDocument,
+    });
+  const products = data.data?.products?.edges.map(({ node }) => {
+    return {
+      name: node.name,
+      description: node.description,
+      image: node.thumbnail?.url,
+      category: node.category?.name,
+      price: node.pricing?.priceRange?.start?.gross.amount,
+    };
+  });
+};
+
+main();
